970ecfb3f281ea5c789b223e6bb0ea0ae54837f8
Uspenskiy Mikhail <s02180053@gse.cs.msu.ru>
2021-11-10 16:26:51 +0000

    Appointment Registration Entity Uploaded

---
+++ src/main/java/com/NetCracker/Entities/AppointmentRegistration.java
@@ -0,0 +1,145 @@
+package com.NetCracker.Entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "appointment_registration")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class AppointmentRegistration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "APPOINTMENT_REGISTRATION_ID", unique = true, nullable = false)
+    private int id;
+
+    @Column(name = "START_DATE_TIME")
+    @NotNull
+    private LocalDateTime start;
+
+    @Column(name = "END_DATE_TIME")
+    private LocalDateTime end;
+
+    @Column(name = "ADDRESS")
+    @NotNull
+    private String address;
+
+    @Column(name = "ROOM")
+    @NotNull
+    private String room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PATIENT", referencedColumnName = "PATIENT_ID")
+    private Patient patient;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DOCTOR", referencedColumnName = "DOCTOR_ID")
+    private Doctor doctor;
+
+    public AppointmentRegistration() {
+    }
+
+    public AppointmentRegistration(LocalDateTime start, LocalDateTime end,
+                                   String address, String room, Patient patient,
+                                   Doctor doctor) {
+        this.start = start;
+        this.end = end;
+        this.address = address;
+        this.room = room;
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AppointmentRegistration)) return false;
+        AppointmentRegistration that = (AppointmentRegistration) o;
+        return getId() == that.getId() && getStart().equals(that.getStart()) &&
+                Objects.equals(getEnd(), that.getEnd()) &&
+                getAddress().equals(that.getAddress()) &&
+                getRoom().equals(that.getRoom()) &&
+                getPatient().equals(that.getPatient()) &&
+                getDoctor().equals(that.getDoctor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getStart(), getEnd(), getAddress(), getRoom(), getPatient(), getDoctor());
+    }
+
+    @Override
+    public String toString() {
+        return "AppointmentRegistration{" +
+                "id=" + id +
+                ", start=" + start +
+                ", end=" + end +
+                ", address='" + address + '\'' +
+                ", room='" + room + '\'' +
+                ", patient=" + patient +
+                ", doctor=" + doctor +
+                '}';
+    }
+}
